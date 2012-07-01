b462b2f8250c0f1502b53af2d8d20f683d807adb
Brennan M. O'Keefe <brennan@considered-harmful.org>
2012-06-30 21:48:09 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '37d3ef8c5b0f906b3e7b5fbc0784f7eb0b5a1a037dbc4cf2a63ee5631fd4f16fc1bbdf0d31d53118629adf86f2220d7ea6bbc3b79b1d9b9040ade9e0b4f254ee'
